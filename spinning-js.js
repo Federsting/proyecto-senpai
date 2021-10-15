7bab72256ff4745d964a72c35f1da6b83a2c897f
Federsting <federicobcastro@hotmail.com>
2021-10-15 14:34:17 -0300

    agrupando archivos

--- spinning-js.js
+++
@@ -1,7 +0,0 @@
-const botones = document.querySelectorAll(".boton");
-const click = function (event) {
-    alert("¡Reserva exitosa!");
-};
-botones.forEach(botones => {
-    botones.addEventListener("click", click)
-});
