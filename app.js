3c918fd9e777b20f2ddd65581c1d8af8f9c06fa7
Federsting <federicobcastro@hotmail.com>
2021-10-21 09:59:24 -0300

    cambios para ver seccion catalogo

--- app.js
+++ app.js
@@ -8,43 +8,43 @@ const port = 4000;
 
 app.use(express.static('public'));
 
-app.get("/index", (req, res, next) => {
+app.get("/index", (req, res,) => {
     res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.get("/menu-principal.html", (req, res, next) => {
+app.get("/menu-principal.html", (req, res) => {
     res.sendFile(path.join(__dirname + "/menu-principal.html"));
 });
 
-app.get("/catalogo.html", (req, res, next) => {
+app.get("/catalogo.html", (req, res) => {
     res.sendFile(path.join(__dirname + "/catalogo.html"));
 });
 
-app.get("/dance-fit.html", (req, res, next) => {
+app.get("/dance-fit.html", (req, res) => {
     res.sendFile(path.join(__dirname + "/dance-fit.html"));
 });
 
-app.get("/funcional.html", (req, res, next) => {
+app.get("/funcional.html", (req, res) => {
     res.sendFile(path.join(__dirname + "/funcional.html"));
 });
 
-app.get("/spinning.html", (req, res, next) => {
+app.get("/spinning.html", (req, res) => {
     res.sendFile(path.join(__dirname + "/spinning.html"));
 });
 
-app.get("/gap.html", (req, res, next) => {
+app.get("/gap.html", (req, res) => {
     res.sendFile(path.join(__dirname + "/gap.html"));
 });
 
-app.get("/olvidaste-la-contrasena.html", (req, res, next) => {
+app.get("/olvidaste-la-contrasena.html", (req, res) => {
     res.sendFile(path.join(__dirname + "/olvidaste-la-contrasena.html"));
 });
 
-app.get("/abonar.html", (req, res, next) => {
+app.get("/abonar.html", (req, res) => {
     res.sendFile(path.join(__dirname + "/abonar.html"));
 });
 
-app.get("/registro.html", (req, res, next) => {
+app.get("/registro.html", (req, res) => {
     res.sendFile(path.join(__dirname + "/registro.html"));
 });
 
